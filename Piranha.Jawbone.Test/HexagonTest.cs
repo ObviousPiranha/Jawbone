8b2b79247b85e59176b97416af44c3c2a61e8786
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2023-01-22 22:40:36 -0700

    Hexagonal fun.

---
+++ Piranha.Jawbone.Test/HexagonTest.cs
@@ -0,0 +1,47 @@
+using Piranha.Jawbone.Hexagon;
+using Xunit;
+
+namespace Piranha.Jawbone.Test;
+
+public class HexagonTest
+{
+    [Fact]
+    public void RotateHexagons()
+    {
+        var axial = new Axial32(11, 17);
+        Assert.Equal(axial, axial.Rotate60().Rotate60().Rotate60().Rotate60().Rotate60().Rotate60());
+        Assert.Equal(axial, axial.Rotate120().Rotate120().Rotate120());
+        Assert.Equal(axial, axial.Rotate180().Rotate180());
+        Assert.Equal(axial, axial.Rotate240().Rotate120());
+        Assert.Equal(axial, axial.Rotate300().Rotate60());
+
+        Assert.Equal(axial.Rotate120(), axial.Rotate60().Rotate60());
+
+        Assert.Equal(axial.Rotate180(), axial.Rotate60().Rotate120());
+        Assert.Equal(axial.Rotate180(), axial.Rotate120().Rotate60());
+        Assert.Equal(axial.Rotate180(), axial.Rotate60().Rotate60().Rotate60());
+
+        Assert.Equal(axial.Rotate240(), axial.Rotate60().Rotate180());
+        Assert.Equal(axial.Rotate240(), axial.Rotate120().Rotate120());
+        Assert.Equal(axial.Rotate240(), axial.Rotate180().Rotate60());
+        Assert.Equal(axial.Rotate240(), axial.Rotate60().Rotate60().Rotate60().Rotate60());
+
+        Assert.Equal(axial.Rotate300(), axial.Rotate60().Rotate240());
+        Assert.Equal(axial.Rotate300(), axial.Rotate120().Rotate180());
+        Assert.Equal(axial.Rotate300(), axial.Rotate180().Rotate120());
+        Assert.Equal(axial.Rotate300(), axial.Rotate240().Rotate60());
+        Assert.Equal(axial.Rotate300(), axial.Rotate60().Rotate60().Rotate60().Rotate60().Rotate60());
+
+        Assert.Equal(axial, axial.Rotate60(0));
+        Assert.Equal(axial, axial.Rotate60(6));
+        Assert.Equal(axial, axial.Rotate60(12));
+        Assert.Equal(axial, axial.Rotate60(-6));
+        Assert.Equal(axial, axial.Rotate60(-12));
+
+        Assert.Equal(axial.Rotate60(), axial.Rotate60(1));
+        Assert.Equal(axial.Rotate120(), axial.Rotate60(2));
+        Assert.Equal(axial.Rotate180(), axial.Rotate60(3));
+        Assert.Equal(axial.Rotate240(), axial.Rotate60(4));
+        Assert.Equal(axial.Rotate300(), axial.Rotate60(5));
+    }
+}
---
+++ Piranha.Jawbone/Hexagon/Axial32.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Piranha.Jawbone.Hexagon;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct Axial32 : IEquatable<Axial32>
+{
+    public readonly int Q { get; }
+    public readonly int R { get; }
+    public readonly int S => -Q - R;
+
+    public Axial32(int q, int r)
+    {
+        Q = q;
+        R = r;
+    }
+
+    public bool Equals(Axial32 other) => Q == other.Q && R == other.R;
+    public override bool Equals([NotNullWhen(true)] object? obj) => obj is Axial32 other && Equals(other);
+    public override int GetHashCode() => HashCode.Combine(Q, R);
+    public override string? ToString() => $"{Q}, {R}, {S}";
+
+    public static bool operator ==(Axial32 a, Axial32 b) => a.Equals(b);
+    public static bool operator !=(Axial32 a, Axial32 b) => !a.Equals(b);
+}
