5bf6c90c5b3646bd4b6fdf8e1c281bf243b7729f
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2023-06-25 23:41:15 -0600

    Add LLA

--- Piranha.Jawbone/Net/Address.cs
+++ Piranha.Jawbone/Net/Address.cs
@@ -16,6 +16,13 @@ public static class Address
         return new(address, port);
     }
 
+    public static LinkLocalAddress128 WithScopeId(
+        this Address128 address,
+        uint scopeId)
+    {
+        return new LinkLocalAddress128(address, scopeId);
+    }
+
     internal static void Swap<T>(ref T a, ref T b) where T : unmanaged
     {
         var c = a;
--- Piranha.Jawbone/Net/IAddress.cs
+++ Piranha.Jawbone/Net/IAddress.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Piranha.Jawbone.Net;
