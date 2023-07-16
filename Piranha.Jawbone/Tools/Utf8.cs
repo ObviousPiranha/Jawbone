5a6dcde2aa1ed871a25394225159bbe9ec7385ac
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2023-07-15 19:29:00 -0600

    Utf8 encoder

--- Piranha.Jawbone/Tools/Utf8.cs
+++ Piranha.Jawbone/Tools/Utf8.cs
@@ -6,8 +6,8 @@ namespace Piranha.Jawbone;
 public static class Utf8
 {
     private const int LeadMask = (1 << 7) | (1 << 6);
-    private const int LeadBit = (1 << 7);
-    private const int SixBits = ~(int.MinValue >> 25);
+    private const int LeadBit = 1 << 7;
+    private const int SixBits = 0x3f;
 
     private static int CountSigBits(int b)
     {
@@ -22,6 +22,58 @@ public static class Utf8
         }
 
         return result;
+    }
+
+    public static int Encode(Span<byte> bytes, int value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value));
+        }
+        else if (value < 0x80)
+        {
+            bytes[0] = (byte)value;
+            return 1;
+        }
+
+        int first;
+        int length;
+
+        if (value < 0x800)
+        {
+            first = 0xc0;
+            length = 2;
+        }
+        else if (value < 0x10000)
+        {
+            first = 0xe0;
+            length = 3;
+        }
+        else if (value < 0x200000)
+        {
+            first = 0xf0;
+            length = 4;
+        }
+        else if (value < 0x4000000)
+        {
+            first = 0xf8;
+            length = 5;
+        }
+        else
+        {
+            first = 0xfc;
+            length = 6;
+        }
+
+        bytes[0] = (byte)(first | (value >> (6 * (length - 1))));
+
+        for (int i = 1; i < length; ++i)
+        {
+            int v = value >> (6 * (length - 1 - i));
+            bytes[i] = (byte)(LeadBit | (v & SixBits));
+        }
+
+        return length;
     }
 
     public static (int codePoint, int length) ReadCodePoint(ReadOnlySpan<byte> utf8)
