f9a7a46cc91f80bba72e438b1052830c1ec4899b
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2023-07-20 19:39:42 -0600

    Formatted.

--- Piranha.Jawbone/Net/UdpSocket.cs
+++ Piranha.Jawbone/Net/UdpSocket.cs
@@ -7,7 +7,7 @@ public static class UdpSocket
     internal const int Reuse = 1 << 0;
     internal const int Bind = 1 << 1;
     internal const int IPv6Only = 1 << 2;
-    
+
     public static int Receive<TAddress>(
         this IUdpSocket<TAddress> socket,
         Span<byte> buffer,
