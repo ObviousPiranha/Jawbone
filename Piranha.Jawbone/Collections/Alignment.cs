8dbc5af7589292b4307f4b77f94cb17b5698f437
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2023-11-19 14:57:53 -0700

    phormatting

--- Piranha.Jawbone/Collections/Alignment.cs
+++ Piranha.Jawbone/Collections/Alignment.cs
@@ -6,13 +6,13 @@ public static class Alignment
 {
     private static class AlignmentInfo<T> where T : unmanaged
     {
-        #pragma warning disable CS0649
+#pragma warning disable CS0649
         private struct Helper
         {
             public byte padding;
             public T target;
         }
-        #pragma warning restore CS0649
+#pragma warning restore CS0649
 
         public static readonly int Value = GetAlignment();
 
