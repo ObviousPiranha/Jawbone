b6cb0c53ad60e6194ea9eed1365bae10032b08a3
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2024-05-06 16:52:44 -0600

    Stuff for world gen.

---
+++ Piranha.Jawbone/Tools/StreamExtensions.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Piranha.Jawbone.Extensions;
+
+public static class StreamExtensions
+{
+    public static T ReadValue<T>(this Stream stream) where T : unmanaged
+    {
+        T result;
+        Unsafe.SkipInit(out result);
+        var n = stream.Read(
+            MemoryMarshal.AsBytes(
+                new Span<T>(ref result)));
+        if (n != Unsafe.SizeOf<T>())
+            throw new InvalidDataException($"Insufficient bytes to read into {typeof(T)}");
+        return result;
+    }
+
+    public static bool TryReadValue<T>(this Stream stream, out T value) where T : unmanaged
+    {
+        Unsafe.SkipInit(out value);
+        var n = stream.Read(
+            MemoryMarshal.AsBytes(
+                new Span<T>(ref value)));
+        return n == Unsafe.SizeOf<T>();
+    }
+
+    public static Stream WriteValue<T>(this Stream stream, in T value) where T : unmanaged
+    {
+        stream.Write(
+            MemoryMarshal.AsBytes(
+                new ReadOnlySpan<T>(in value)));
+        return stream;
+    }
+}
