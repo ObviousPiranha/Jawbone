5543fca2bcef94ef5aec42bccfe3e4057102e438
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2024-09-01 14:51:06 -0600

    OK, xunit.

--- source/Piranha.Jawbone.Test/AddressTest.cs
+++ source/Piranha.Jawbone.Test/AddressTest.cs
@@ -32,66 +32,66 @@ public class AddressTest
 
     [Theory]
     [MemberData(nameof(LinkLocal32))]
-    public void AddressV4_IsLinkLocal(AddressV4 address)
+    public void AddressV4_IsLinkLocal(Serializable<AddressV4> address)
     {
-        Assert.True(address.IsLinkLocal);
+        Assert.True(address.Value.IsLinkLocal);
     }
 
     [Theory]
     [MemberData(nameof(NotLinkLocal32))]
-    public void AddressV4_IsNotLinkLocal(AddressV4 address)
+    public void AddressV4_IsNotLinkLocal(Serializable<AddressV4> address)
     {
-        Assert.False(address.IsLinkLocal);
+        Assert.False(address.Value.IsLinkLocal);
     }
 
     [Theory]
     [MemberData(nameof(LinkLocal128))]
-    public void AddressV6_IsLinkLocal(AddressV6 address)
+    public void AddressV6_IsLinkLocal(Serializable<AddressV6> address)
     {
-        Assert.True(address.IsLinkLocal);
+        Assert.True(address.Value.IsLinkLocal);
     }
 
     [Theory]
     [MemberData(nameof(NotLinkLocal128))]
-    public void AddressV6_IsNotLinkLocal(AddressV6 address)
+    public void AddressV6_IsNotLinkLocal(Serializable<AddressV6> address)
     {
-        Assert.False(address.IsLinkLocal);
+        Assert.False(address.Value.IsLinkLocal);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse32))]
-    public void AddressV4_RoundTripParseString(AddressV4 expected)
+    public void AddressV4_RoundTripParseString(Serializable<AddressV4> expected)
     {
         var asString = expected.ToString();
         var actual = AddressV4.Parse(asString, null);
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse32))]
-    public void AddressV4_RoundTripParseSpan(AddressV4 expected)
+    public void AddressV4_RoundTripParseSpan(Serializable<AddressV4> expected)
     {
         var asString = expected.ToString();
         var actual = AddressV4.Parse(asString.AsSpan(), null);
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse32))]
-    public void AddressV4_RoundTripTryParseString(AddressV4 expected)
+    public void AddressV4_RoundTripTryParseString(Serializable<AddressV4> expected)
     {
         var asString = expected.ToString();
         Assert.True(AddressV4.TryParse(asString, null, out var actual));
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse32))]
-    public void AddressV4_RoundTripTryParseSpan(AddressV4 expected)
+    public void AddressV4_RoundTripTryParseSpan(Serializable<AddressV4> expected)
     {
         var asString = expected.ToString();
         Assert.True(AddressV4.TryParse(asString.AsSpan(), null, out var actual));
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
@@ -112,38 +112,38 @@ public class AddressTest
 
     [Theory]
     [MemberData(nameof(RoundTripParse128))]
-    public void AddressV6_RoundTripParseString(AddressV6 expected)
+    public void AddressV6_RoundTripParseString(Serializable<AddressV6> expected)
     {
         var asString = expected.ToString();
         var actual = AddressV6.Parse(asString, null);
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse128))]
-    public void AddressV6_RoundTripParseSpan(AddressV6 expected)
+    public void AddressV6_RoundTripParseSpan(Serializable<AddressV6> expected)
     {
         var asString = expected.ToString();
         var actual = AddressV6.Parse(asString.AsSpan(), null);
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse128))]
-    public void AddressV6_RoundTripTryParseString(AddressV6 expected)
+    public void AddressV6_RoundTripTryParseString(Serializable<AddressV6> expected)
     {
         var asString = expected.ToString();
         Assert.True(AddressV6.TryParse(asString, null, out var actual));
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
     [MemberData(nameof(RoundTripParse128))]
-    public void AddressV6_RoundTripTryParseSpan(AddressV6 expected)
+    public void AddressV6_RoundTripTryParseSpan(Serializable<AddressV6> expected)
     {
         var asString = expected.ToString();
         Assert.True(AddressV6.TryParse(asString.AsSpan(), null, out var actual));
-        Assert.Equal(expected, actual);
+        Assert.Equal(expected.Value, actual);
     }
 
     [Theory]
@@ -161,41 +161,41 @@ public class AddressTest
         Assert.Throws<FormatException>(() => AddressV6.Parse(s.AsSpan(), null));
     }
 
-    public static TheoryData<AddressV4> LinkLocal32 => new()
+    public static TheoryData<Serializable<AddressV4>> LinkLocal32 => new()
     {
-        new(169, 254, 0, 0),
-        new(169, 254, 127, 127),
-        new(169, 254, 255, 255)
+        new AddressV4(169, 254, 0, 0),
+        new AddressV4(169, 254, 127, 127),
+        new AddressV4(169, 254, 255, 255)
     };
 
-    public static TheoryData<AddressV4> NotLinkLocal32 => new()
+    public static TheoryData<Serializable<AddressV4>> NotLinkLocal32 => new()
     {
         AddressV4.Local,
         AddressV4.Broadcast,
-        new(169, 200, 0, 0),
-        new(170, 254, 0, 0)
+        new AddressV4(169, 200, 0, 0),
+        new AddressV4(170, 254, 0, 0)
     };
 
-    public static TheoryData<AddressV6> LinkLocal128 => new()
+    public static TheoryData<Serializable<AddressV6>> LinkLocal128 => new()
     {
         MakeLinkLocal()
     };
 
-    public static TheoryData<AddressV6> NotLinkLocal128 => new()
+    public static TheoryData<Serializable<AddressV6>> NotLinkLocal128 => new()
     {
         AddressV6.Local,
         Create(static span => span.Fill(0xab))
     };
 
-    public static TheoryData<AddressV4> RoundTripParse32 => new()
+    public static TheoryData<Serializable<AddressV4>> RoundTripParse32 => new()
     {
         AddressV4.Local,
         AddressV4.Broadcast,
-        new(192, 168, 0, 1),
-        new(0, 1, 2, 3)
+        new AddressV4(192, 168, 0, 1),
+        new AddressV4(0, 1, 2, 3)
     };
 
-    public static TheoryData<AddressV6> RoundTripParse128 => new()
+    public static TheoryData<Serializable<AddressV6>> RoundTripParse128 => new()
     {
         AddressV6.Local,
         Create(static span => span.Fill(0xab)),
