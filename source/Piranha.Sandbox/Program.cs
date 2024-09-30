c6c7ec6902571f4bec00cb3a02a1418b8ebd6558
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2024-09-29 23:26:33 -0600

    I learned moar.

--- source/Piranha.Sandbox/Program.cs
+++ source/Piranha.Sandbox/Program.cs
@@ -27,8 +27,26 @@ class Program
 
     static void BindTest()
     {
-        using var socketA = UdpSocketV6.BindAnyIp(7777);
-        using var socketB = UdpSocketV4.BindAnyIp(7777);
+        var port = 7777;
+        using var socket6A = UdpSocketV6.BindAnyIp(port);
+        using var socket4A = UdpSocketV4.BindAnyIp(port);
+
+        using var socket6B = UdpSocketV6.Create();
+        using var socket4B = UdpSocketV4.Create();
+
+        socket6B.Send("IPv6"u8, AddressV6.Local.OnPort(port));
+        socket4B.Send("IPv4"u8, AddressV4.Local.OnPort(port));
+
+        var buffer = new byte[64];
+        var timeout = TimeSpan.FromSeconds(1);
+        socket6A.Receive(buffer, timeout, out var result6);
+        result6.ThrowOnErrorOrTimeout();
+        Console.WriteLine(Encoding.UTF8.GetString(buffer.AsSpan(0, result6.ReceivedByteCount)));
+
+        socket4A.Receive(buffer, timeout, out var result4);
+        result4.ThrowOnErrorOrTimeout();
+        Console.WriteLine(Encoding.UTF8.GetString(buffer.AsSpan(0, result4.ReceivedByteCount)));
+
         Console.WriteLine("Huh. Wow.");
     }
 
