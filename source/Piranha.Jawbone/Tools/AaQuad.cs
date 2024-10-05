147edd063b9737704046f44a7141f3497948f02e
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2024-10-04 21:04:39 -0600

    Support vec3 quad conversion.

--- source/Piranha.Jawbone/Tools/AaQuad.cs
+++ source/Piranha.Jawbone/Tools/AaQuad.cs
@@ -24,12 +24,23 @@ public struct AaQuad<T> // axis-aligned quad
 
 public static class AaQuad
 {
+    public static AaQuad<T> Create<T>(T a, T c) => new(a, c);
     public static Quad<Vector2> ToQuad(this AaQuad<Vector2> aaq)
     {
         return new Quad<Vector2>(
             aaq.A,
-            new Vector2(aaq.A.X, aaq.C.Y),
+            new(aaq.A.X, aaq.C.Y),
             aaq.C,
-            new Vector2(aaq.C.X, aaq.A.Y));
+            new(aaq.C.X, aaq.A.Y));
+    }
+
+    public static Quad<Vector3> ToQuad(this AaQuad<Vector3> aaq)
+    {
+        var z = aaq.A.Z;
+        return new Quad<Vector3>(
+            aaq.A,
+            new(aaq.A.X, aaq.C.Y, z),
+            aaq.C,
+            new(aaq.C.X, aaq.A.Y, z));
     }
 }
