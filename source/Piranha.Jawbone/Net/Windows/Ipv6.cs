580dfb043aec771cfb58fdbe3aacb7dd4fe73b48
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2025-05-04 15:44:22 -0600

    Formatting.

--- source/Piranha.Jawbone/Net/Windows/Ipv6.cs
+++ source/Piranha.Jawbone/Net/Windows/Ipv6.cs
@@ -9,12 +9,12 @@ static class Ipv6
     public static void SetIpv6Only(nuint socket, bool allowV4)
     {
         uint yes = allowV4 ? 0u : 1u;
-            var result = Sys.SetSockOpt(
-            socket,
-            IpProto.Ipv6,
-            V6Only,
-            yes,
-            Unsafe.SizeOf<uint>());
+        var result = Sys.SetSockOpt(
+        socket,
+        IpProto.Ipv6,
+        V6Only,
+        yes,
+        Unsafe.SizeOf<uint>());
 
         if (result == -1)
             Sys.Throw("Unable to set socket option.");
