911ab6f942249678f05100cea78890909761ab01
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2026-04-19 11:46:38 -0600

    Erase sandbox.

--- source/Piranha.Sandbox/Program.cs
+++ source/Piranha.Sandbox/Program.cs
@@ -13,25 +13,6 @@ class Program
     {
         try
         {
-            Console.WriteLine(new KeyMapping(SdlScancode.Kp7, KeyModifier.Alt));
-            Console.WriteLine(new KeyMapping(SdlScancode.A));
-            Console.WriteLine(new KeyMapping(SdlScancode.A, KeyModifier.Shift));
-
-            var mappings = new Dictionary<KeyMapping, string>
-            {
-                [new KeyMapping(SdlScancode.A, KeyModifier.Control)] = "Huzzah"
-            };
-
-            var mappingsJson = JsonSerializer.Serialize(mappings);
-
-            var keyMapping = new KeyMapping(SdlScancode.Kp7, KeyModifier.Control | KeyModifier.Alt);
-            var serialized = JsonSerializer.SerializeToUtf8Bytes(keyMapping);
-            Console.WriteLine(Encoding.UTF8.GetString(serialized));
-            var deserialized = JsonSerializer.Deserialize<KeyMapping?>(serialized);
-            Console.WriteLine(deserialized.ToString());
-
-            var nullJson = "null"u8;
-            var danger = JsonSerializer.Deserialize<KeyMapping?>(nullJson);
         }
         catch (Exception ex)
         {
