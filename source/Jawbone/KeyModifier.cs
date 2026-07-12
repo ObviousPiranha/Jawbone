d3e1d0376152407f5f14c47af50185ace071bf2f
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2026-07-12 15:14:54 -0600

    KeyModifier extension.

--- source/Jawbone/KeyModifier.cs
+++ source/Jawbone/KeyModifier.cs
@@ -16,4 +16,17 @@ public static class KeyModifierExtensions
 {
     public static bool MaskAll(this KeyModifier keyModifier, KeyModifier mask) => (keyModifier & mask) == mask;
     public static bool MaskAny(this KeyModifier keyModifier, KeyModifier mask) => (keyModifier & mask) != KeyModifier.None;
+    public static KeyModifier Create(Sdl3.SdlKeymod sdlKeymod)
+    {
+        var result = KeyModifier.None;
+        if ((sdlKeymod & Sdl3.SdlKeymod.Ctrl) != Sdl3.SdlKeymod.None)
+            result |= KeyModifier.Control;
+        if ((sdlKeymod & Sdl3.SdlKeymod.Shift) != Sdl3.SdlKeymod.None)
+            result |= KeyModifier.Shift;
+        if ((sdlKeymod & Sdl3.SdlKeymod.Alt) != Sdl3.SdlKeymod.None)
+            result |= KeyModifier.Alt;
+        if ((sdlKeymod & Sdl3.SdlKeymod.Gui) != Sdl3.SdlKeymod.None)
+            result |= KeyModifier.Super;
+        return result;
+    }
 }
