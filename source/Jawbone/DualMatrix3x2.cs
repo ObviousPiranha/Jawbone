eefe697fcbe482f07dee50e2cc71762f81d4257c
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2026-07-18 11:56:15 -0600

    Simplified DualMatrix3x2.

--- source/Jawbone/DualMatrix3x2.cs
+++ source/Jawbone/DualMatrix3x2.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 
 namespace Jawbone;
 
@@ -15,57 +13,50 @@ public readonly struct DualMatrix3x2
         Inverse = inverse;
     }
 
-    public DualMatrix3x2 Transform(Matrix3x2 forward, Matrix3x2 inverse) =>
-        new(Forward * forward, inverse * Inverse);
-
-    public DualMatrix3x2 Transform(DualMatrix3x2 dualMatrix) =>
-        Transform(dualMatrix.Forward, dualMatrix.Inverse);
-
-    public DualMatrix3x2 Scale(float x, float y)
+    public static DualMatrix3x2 CreateRotation(float radians)
     {
-        Validate(x);
-        Validate(y);
-
-        return Transform(
-            Matrix3x2.CreateScale(x, y),
-            Matrix3x2.CreateScale(1f / x, 1f / y));
-
-        static void Validate(
-            float n,
-            [CallerArgumentExpression(nameof(n))] string? paramName = null)
-        {
-            if (!float.IsRealNumber(n) || float.Abs(n) < float.Epsilon)
-                throw new ArgumentException("Scale factor must be real non-zero value.", paramName);
-        }
-    }
-
-    public DualMatrix3x2 Scale(float xy) => Scale(xy, xy);
-
-    public DualMatrix3x2 Scale(Vector2 v) => Scale(v.X, v.Y);
-
-    public DualMatrix3x2 Translate(float x, float y)
-    {
-        return Transform(
-            Matrix3x2.CreateTranslation(x, y),
-            Matrix3x2.CreateTranslation(-x, -y));
-    }
-
-    public DualMatrix3x2 Translate(Vector2 v) => Translate(v.X, v.Y);
-
-    public DualMatrix3x2 Rotate(float radians)
-    {
-        return Transform(
+        return new(
             Matrix3x2.CreateRotation(radians),
             Matrix3x2.CreateRotation(-radians));
     }
 
-    public DualMatrix3x2 Rotate(float radians, Vector2 centerPoint)
+    public static DualMatrix3x2 CreateRotation(float radians, Vector2 centerPoint)
     {
-        return Transform(
+        return new(
             Matrix3x2.CreateRotation(radians, centerPoint),
             Matrix3x2.CreateRotation(-radians, centerPoint));
     }
 
-    public static DualMatrix3x2 Create() =>
-        new(Matrix3x2.Identity, Matrix3x2.Identity);
+    public static DualMatrix3x2 CreateScale(float scale)
+    {
+        return new(
+            Matrix3x2.CreateScale(scale),
+            Matrix3x2.CreateScale(1f / scale));
+    }
+
+    public static DualMatrix3x2 CreateScale(float xScale, float yScale)
+    {
+        return new(
+            Matrix3x2.CreateScale(xScale, yScale),
+            Matrix3x2.CreateScale(1f / xScale, 1f / yScale));
+    }
+
+    public static DualMatrix3x2 CreateScale(Vector2 scales) => CreateScale(scales.X, scales.Y);
+
+    public static DualMatrix3x2 CreateTranslation(float xPosition, float yPosition)
+    {
+        return new(
+            Matrix3x2.CreateTranslation(xPosition, yPosition),
+            Matrix3x2.CreateTranslation(-xPosition, -yPosition));
+    }
+
+    public static DualMatrix3x2 CreateTranslation(Vector2 position)
+    {
+        return new(
+            Matrix3x2.CreateTranslation(position),
+            Matrix3x2.CreateTranslation(-position));
+    }
+    
+    public static DualMatrix3x2 operator *(DualMatrix3x2 a, DualMatrix3x2 b) =>
+        new(a.Forward * b.Forward, b.Inverse * a.Inverse);
 }
